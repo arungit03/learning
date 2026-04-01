e155169690d7a71ecc749a9e7f493697aa4f5cbe
Arunkumar <arun182007@gmail.com>
2026-04-01 20:30:31 +0530

    Create setup.py

---
+++ python/numpy/day1/setup.py
@@ -0,0 +1,21 @@
+# NumPy means Numerical Python
+
+# It is used for:
+#   => arrays
+#   => math operations
+#   => fast calculation
+
+# install numpy command
+
+#  pip install numpy
+
+import numpy as np 
+
+# numpy is library name
+# np is short name
+
+# Create arrays 
+
+
+a=np.array([1,2,3,])
+print(a)
